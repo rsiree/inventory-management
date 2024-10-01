138a00da422a6a372aa255a083c497031dfdafce
rsiree <ravulapalli.sireesha.official@gmail.com>
2024-10-01 18:47:35 +0530

    api integrate, comment url

--- inventory-management/src/App.tsx
+++ inventory-management/src/App.tsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { TbLogout } from "react-icons/tb";
 import { FaShoppingCart } from "react-icons/fa";
 import { RiExchangeDollarLine } from "react-icons/ri";
 import { MdRemoveShoppingCart, MdCategory, MdDelete, MdEdit } from "react-icons/md";
 import { IoEyeSharp } from "react-icons/io5";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from './redux/slices/data';
 
 function App() {
+
+  const dispatch = useDispatch<any>();
+  const { data, isLoading, categoryData, outOfStock, totalStoreValue } = useSelector((state: any) => state.dataReducer);
   const [isAdmin, setIsAdmin] = useState(false);
 
+
+  useEffect(() => {
+    dispatch(fetchData())
+  }, []);
+
+  if (isLoading) return <div className='text-2xl m-auto text-center'>Loading ...</div>
+
   return (
-    <div className="container py-3">
+    <div className="max-w-7xl py-3 px-4">
 
       {/* navigation */}
       <div className='flex justify-end gap-10 items-center'>
@@ -30,42 +42,42 @@ function App() {
       </div>
 
       {/* inventory stats */}
-      <div className='max-w-7xl m-auto'>
+      <div className='m-auto'>
         <h2 className='text-2xl text-primary m-4 mb-6'>Inventory Stats</h2>
         <div className='grid grid-cols-4 gap-4'>
           <div className='bg-inventory-card-background p-4 items-center rounded-md flex gap-4 align-top'>
             <FaShoppingCart className='w-8 h-8' />
             <div className='flex flex-col items-start'>
               <p className='text-sm'>Total Product</p>
-              <p className='text-2xl font-medium'>9</p>
+              <p className='text-2xl font-medium'>{data?.length}</p>
             </div>
           </div>
           <div className='bg-inventory-card-background p-3 rounded-md flex gap-4 align-top min-h-24'>
             <RiExchangeDollarLine className='w-8 h-8' />
             <div className='flex flex-col items-start'>
               <p className='text-sm'>Total Store Value</p>
-              <p className='text-2xl font-medium'>9</p>
+              <p className='text-2xl font-medium'>{totalStoreValue ?? 0}</p>
             </div>
           </div>
           <div className='bg-inventory-card-background p-3 rounded-md flex gap-4 align-top min-h-24'>
             <MdRemoveShoppingCart className='w-8 h-8' />
             <div className='flex flex-col items-start'>
               <p className='text-sm'>Out of Stocks</p>
-              <p className='text-2xl font-medium'>9</p>
+              <p className='text-2xl font-medium'>{outOfStock ?? 0}</p>
             </div>
           </div>
           <div className='bg-inventory-card-background p-3 rounded-md flex gap-4 align-top min-h-24'>
             <MdCategory className='w-8 h-8' />
             <div className='flex flex-col items-start'>
               <p className='text-sm'>No of Category</p>
-              <p className='text-2xl font-medium'>9</p>
+              <p className='text-2xl font-medium'>{categoryData ? Object.keys(categoryData)?.length : 0}</p>
             </div>
           </div>
         </div>
       </div>
 
       {/* data mapping */}
-      <table className='bg-table-background w-full max-w-screen-xl m-auto mt-7 rounded-md'>
+      <table className='bg-table-background w-full m-auto mt-7 rounded-md'>
         <thead className='w-full'>
           <tr>
             <th className=' text-table-title text-sm p-4 font-normal '><div className='w-fit h-fit bg-background rounded-lg p-2 py-1'>Name</div></th>
@@ -76,19 +88,22 @@ function App() {
             <th className='text-table-title text-sm p-4 font-normal '><div className='w-fit h-fit bg-background rounded-lg p-2 py-1'>Actions</div></th>
           </tr>
         </thead>
-        <tbody className='min-h-96 w-full border-t-slate-200 border-t-[1px]'>
-          <tr className='mt-2'>
-            <td className='text-base pt-2'>Bluetooth</td>
-            <td className="text-base">Electronic</td>
-            <td className="text-base">50</td>
-            <td className="text-base">1</td>
-            <td className="text-base">1</td>
-            <td className="text-base flex gap-1">
-              <MdEdit className='text-green-800' />
-              <IoEyeSharp className='text-purple-500' />
-              <MdDelete className='text-red-600' />
-            </td>
-          </tr>
+        <tbody className='w-full pb-2 border-b-slate-200 border-b-[1px]'>
+          {data?.map((item: any, index: number) => (
+            <tr className='mt-2' key={index}>
+              <td className='text-base px-4 py-4 border-border border-t-[1px]'>{item?.name}</td>
+              <td className='text-base px-4 py-4 border-border border-t-[1px]'>{item?.category}</td>
+              <td className='text-base px-4 py-4 border-border border-t-[1px]'>{item?.price}</td>
+              <td className='text-base px-4 py-4 border-border border-t-[1px]'>{item?.quantity}</td>
+              <td className='text-base px-4 py-4 border-border border-t-[1px]'>{item?.value}</td>
+              <td className="text-base px-4 py-5 border-border border-t-[1px]
+               flex gap-1">
+                <MdEdit className='text-green-800' />
+                <IoEyeSharp className='text-purple-500' />
+                <MdDelete className='text-red-600' />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
 
--- inventory-management/src/redux/slices/data.ts
+++ inventory-management/src/redux/slices/data.ts
@@ -2,17 +2,52 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 interface intialStateProps {
-
+    data: Array<Object>,
+    isLoading: Boolean,
+    categoryData: any,
+    outOfStock: number,
+    totalStoreValue: 0
 }
 
 const initialState: intialStateProps = {
-
+    data: [],
+    isLoading: false,
+    categoryData: null,
+    outOfStock: 0,
+    totalStoreValue: 0
 }
 
+// const url = process.env.BASE_URL || 'https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory';
 export const fetchData = createAsyncThunk(
     'fetchData',
     async () => {
-        const response = await axios.get('')
+        const response = await axios.get('');
+        // console.log(response);
+        // const data = response.data;
+        const data = [{ "name": "Bluetooth", "category": "Electronic", "value": "$150", "quantity": 5, "price": "$30" }, { "name": "Edifier M43560", "category": "Electronic", "value": "0", "quantity": 0, "price": "$0" }, { "name": "Sony 4k ultra 55 inch TV", "category": "Electronic", "value": "$1190", "quantity": 17, "price": "$70" }, { "name": "Samsumg 55 inch TV", "category": "Electronic", "value": "$600", "quantity": 50, "price": "$12" }, { "name": "samsumg S34 Ultra", "category": "phone", "value": "$0", "quantity": 0, "price": "$0" }]
+
+        let out_of_stock = 0;
+        const categories = {} as any;
+        let total_store_value = 0;
+
+        for (let i = 0; i < data?.length; i++) {
+            if (data[i]?.quantity === 0) out_of_stock++;
+
+            for (let key in data[i]) {
+                if (key === 'value') {
+                    let store_value = data[i]?.value?.split("$")?.[1] || data[i]?.value;
+                    total_store_value += Number(store_value);
+                }
+
+                if (key === 'category') {
+                    let categoryName = data[i]?.category;
+                    if (categories[categoryName] === undefined) categories[categoryName] = 1;
+                    else categories[categoryName] += 1
+                }
+            }
+        }
+
+        return { data, categories, out_of_stock, total_store_value };
     }
 )
 
@@ -25,13 +60,18 @@ export const dataSlice = createSlice({
     extraReducers: (builder) => {
         builder
             .addCase(fetchData.pending, (state: any, action: any) => {
-
+                state.isLoading = true;
             })
             .addCase(fetchData.fulfilled, (state: any, action: any) => {
-
+                const { data, categories, out_of_stock, total_store_value } = action?.payload;
+                state.isLoading = false;
+                state.data = data;
+                state.categoryData = categories;
+                state.outOfStock = out_of_stock;
+                state.totalStoreValue = total_store_value
             })
             .addCase(fetchData.rejected, (state: any, action: any) => {
-
+                state.isLoading = false
             })
     }
 })
